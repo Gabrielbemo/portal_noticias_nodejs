9eb945dbef54f620a40519b918f264393aa2c559
Gabrielbemo <gabrielbemoura@gmail.com>
2018-08-06 13:33:38 -0300

    Enviando e recebendo parâmetros com resquest.query dos id's das noticias

--- app/controllers/noticias.js
+++ app/controllers/noticias.js
@@ -13,7 +13,9 @@ module.exports.noticia = function (app, req, res) {
     var connection = app.config.dbConnection();
     var noticiasModel = new app.app.models.NoticiasDAO(connection);
 
-    noticiasModel.getNoticia(function (erro, result) {
+    var id_noticia = req.query;
+
+    noticiasModel.getNoticia(id_noticia, function (erro, result) {
         res.render('noticias/noticia', { noticia: result });
     });
 }
